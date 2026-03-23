1e496f9ebabf69bf314ca9ba764ef73617b8d300
hoarfrost31 <nkapse45@gmail.com>
2026-03-24 00:59:50 +0530

    Add icon to completed milestone badge

--- js/milestones.js
+++ js/milestones.js
@@ -94,7 +94,7 @@ function renderMilestones(practiceDates = []) {
             <span>${milestone.desc}</span>
           </div>
           <span class="badge-text">
-            ${isCompleted ? "Completed" : isCurrent ? '<i data-lucide="activity"></i><span>In Progress</span>' : '<i data-lucide="lock"></i><span>Locked</span>'}
+            ${isCompleted ? '<i data-lucide="check-circle-2"></i><span>Completed</span>' : isCurrent ? '<i data-lucide="activity"></i><span>In Progress</span>' : '<i data-lucide="lock"></i><span>Locked</span>'}
           </span>
         </div>
 
